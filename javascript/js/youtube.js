0c95b58e87ca09db8713442ccea416171823e68c
Reza Asghari <reza_asghari83@yahoo.com>
2021-04-19 01:33:07 +0430

    worked on basic of variables and comments in js

--- javascript/js/youtube.js
+++ javascript/js/youtube.js
@@ -1,5 +1,7 @@
-document.getElementById("button").onclick = function () {
-  document.getElementById("confirm").innerHTML =
-    "Order placed. Chech email for confirmation";
-  document.getElementById("button").style.display = "none";
-};
+// document.getElementById("button").onclick = function () {
+//   document.getElementById("confirm").innerHTML =
+//     "Order placed. Chech email for confirmation";
+//   document.getElementById("button").style.display = "none";
+// };
+var username = prompt();
+console.log(username);
