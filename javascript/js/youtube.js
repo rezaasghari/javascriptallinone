4d164fb39e4f9224fea3bf1fce2c57c62d1ac789
Reza Asghari <reza_asghari83@yahoo.com>
2021-04-19 02:07:23 +0430

    worked on variables and expressions on javascripts

--- javascript/js/youtube.js
+++ javascript/js/youtube.js
@@ -3,5 +3,17 @@
 //     "Order placed. Chech email for confirmation";
 //   document.getElementById("button").style.display = "none";
 // };
-var username = prompt();
-console.log(username);
+//var username = prompt();
+// username = 5;
+// username = "Hello";
+// console.log(username);
+// var age = 50;
+// console.log(age);
+// console.log(age);
+// console.log(age);
+var age = 50;
+var newAge = age;
+var message = "newAge = " + newAge; //"newAge = 50"
+console.log(message);
+//declaration
+//initialization
