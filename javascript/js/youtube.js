044185fd35ec8c6d923cc332fe385a168f26d591
Reza Asghari <reza_asghari83@yahoo.com>
2021-05-03 02:43:28 +0430

    worked on decimal and octal and hex binary in js

--- javascript/js/youtube.js
+++ javascript/js/youtube.js
@@ -88,9 +88,23 @@
 //   slicesOfPizza += 10;
 //   console.log(slicesOfPizza);
 // }
+// {
+//   let x = 5;
+//   let y = "10";
+//   let yInt = Number.parseFloat("10.99999999");
+//   console.log(x + yInt);
+// }
+// {
+//   var input = prompt("Put in a number yo");
+//   console.log("Decimal  :", input);
+//   console.log("Binary   :", Number.parseInt(input, 2));
+//   console.log("Octal    :", Number.parseInt(input, 8));
+//   console.log("Hex      :", Number.parseInt(input, 16));
+// }
 {
-  let x = 5;
-  let y = "10";
-  let yInt = Number.parseFloat("10.99999999");
-  console.log(x + yInt);
+  var input = Number(prompt("Put in a DECIMAL number yo"));
+  console.log(input, "in decimal to decimal:", input);
+  console.log(input, "in decimal to binary :", input.toString(2));
+  console.log(input, "in decimal to octal  :", input.toString(8));
+  console.log(input, "in decimal to hex    :", input.toString(16));
 }
