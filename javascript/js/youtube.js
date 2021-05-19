bf864d2bad561ba541229f5259f088514de4cdd8
Reza Asghari <reza_asghari83@yahoo.com>
2021-05-20 00:26:14 +0430

    worked on math objects a little bit more

--- javascript/js/youtube.js
+++ javascript/js/youtube.js
@@ -108,19 +108,29 @@
 //   console.log(input, "in decimal to octal  :", input.toString(8));
 //   console.log(input, "in decimal to hex    :", input.toString(16));
 // }
+// {
+// let x = new Number();
+// Number.
+// let x = 5;
+// console.log(x.toExponential(5));
+// let x = 5959595959;
+// console.log(x.toExponential(3));
+// let x = 5.59595959;
+// console.log("$" + x.toFixed(2));
+// let x = 559595959;
+// console.log("$" + x.toLocaleString());
+// let x = 559595959;
+// console.log("$" + x.toPrecision(2));
+// let x = new Number(559595959);
+// console.log(typeof x);
+// }
 {
-  // let x = new Number();
-  // Number.
-  // let x = 5;
-  // console.log(x.toExponential(5));
-  // let x = 5959595959;
-  // console.log(x.toExponential(3));
-  // let x = 5.59595959;
-  // console.log("$" + x.toFixed(2));
-  // let x = 559595959;
-  // console.log("$" + x.toLocaleString());
-  // let x = 559595959;
-  // console.log("$" + x.toPrecision(2));
-  let x = new Number(559595959);
-  console.log(typeof x);
+  var abs = Math.abs(-36); //36
+  var goUp = Math.ceil(0.00000001); //1
+  var goDown = Math.floor(0.99999); //0
+  var powerUp = Math.pow(3, 2); //9
+  var roundUp = Math.round(4.9); //5
+  var roundDown = Math.round(5.1); //5
+  var isPositive = Math.sign(-Infinity); //-1 (false)
+  var getInt = Math.trunc(4.99999); //4
 }
